e3d6d36dea47f72a4bff1c6fb54156cc54d38f51
khushisingh91999-png <khushisingh91999@gmail.com>
2026-08-15 16:32:05 +0530

    added express

--- index.js
+++ index.js
@@ -12,15 +12,12 @@ app.get('/packages', (req, res) => {
     res.json(packages);
 });
 
-// use http://localhost:3000/packages in postman to get all the packages --------------------------------
 
 app.get('/packages/:id', (req, res) => {
     const packageId = parseInt(req.params.id);
     const selectedPackage = packages.find(pkg => pkg.id === packageId); // comparing what id we put in postman with the id in the packages array, if true then it will return the package with that id
     res.json(selectedPackage);
 });
-// use http://localhost:3000/packages/1 in postman to get the package with id 1 here
-// --------------------------------
 
 const PORT = process.env.PORT || 3000;
 
