4a64bfb712851d98e07c50ea7294168ed4d2009c
khushisingh91999-png <khushisingh91999@gmail.com>
2026-08-17 14:01:17 +0530

    added tour

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ app.get('/packages', (req, res) => {
 
 app.get('/packages/:id', (req, res) => {
     const packageId = parseInt(req.params.id);
-    const selectedPackage = packages.find(pkg => pkg.id === packageId); // comparing what id we put in postman with the id in the packages array, if true then it will return the package with that id
+    const selectedPackage = packages.find(pkg => pkg.id === packageId);
     res.json(selectedPackage);
 });
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
 
