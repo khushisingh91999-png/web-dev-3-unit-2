a0bfa6fa81a4d6cccaf5d1a301babd00a325624a
khushisingh91999-png <khushisingh91999@gmail.com>
2026-08-19 13:44:40 +0530

    added index

--- index.js
+++ index.js
@@ -12,15 +12,13 @@ app.get('/packages', (req, res) => {
     res.json(packages);
 });
 
-// use http://localhost:3000/packages in postman to get all the packages --------------------------------
 
 app.get('/packages/:id', (req, res) => {
     const packageId = parseInt(req.params.id);
     const selectedPackage = packages.find(pkg => pkg.id === packageId); // comparing what id we put in postman with the id in the packages array, if true then it will return the package with that id
     res.json(selectedPackage);
 });
-// use http://localhost:3000/packages/1 in postman to get the package with id 1 here
-// --------------------------------
+
 
 const PORT = process.env.PORT || 3000;
 
@@ -28,5 +26,3 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-// query parameters are used to filter the data based on certain criteria. In this case, we can use query parameters to filter the packages based on the destination. For example, if we want to get all the packages that have a destination of "Maldives", we can use the following URL: http://localhost:3000/packages?des=Maldives
-// http://localhost:3000/packages?des=maldieves
