51dc15807c83bccfad197551a691997122418c2d
khushisingh91999-png <khushisingh91999@gmail.com>
2026-09-03 12:21:08 +0530

    added middleware

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const app = express();
 
-const tourRoutes = require('./routes/tourRoutes');
+//const tourRoutes = require('./routes/tourRoutes');
 
 app.use(express.json());
-app.use('/api', tourRoutes);
+//app.use('/api', tourRoutes);
 
 // const packages = require('./data/tour');
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+// app.get('/', (req, res) => {
+//     res.send('Hello World');
+// });
 
 
 // app.get('/packages', (req, res) => {
@@ -26,6 +26,35 @@ app.get('/', (req, res) => {
 
 
 // const PORT = process.env.PORT || 3000;
+
+
+// client -> Request -> Middleware1 -> Middleware2 -> Middleware3 -> Response -> client
+
+// const middleware1 = (req, res, next) => {
+//     console.log('Middleware 1');
+//     next();
+// }
+
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+}
+
+app.use(logger);
+
+
+const checkAge = (req,res,next)=>{
+  const age = 6;
+  if(age < 18){
+    return res.status(403).json({message:"You are not allowed to access this resource"});
+  }
+  next();
+};
+app.use(checkAge);
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
 
 app.listen(3000, () => {
   console.log(`Server is running on port 3000 `);
